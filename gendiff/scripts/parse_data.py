145aae4d3d92296cfe4326c3fc554c7cfbff5a93
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2026-02-01 21:17:29 +0300

    added function in parse_data.py which returns a tree with categorized data

--- gendiff/scripts/parse_data.py
+++ gendiff/scripts/parse_data.py
@@ -34,90 +34,40 @@ def read_files(file1, file2):
     return data_1, data_2
 
 
-def find_the_same_data(data_1, data_2):
+def get_tree_with_categories(before, after):
+	accumulator = {}
+	before_keys = set(before.keys())
+	after_keys = set(after.keys())
+	removed_keys = before_keys - after_keys
+	added_keys = after_keys - before_keys
 
-    keys_1 = data_1.keys()
-    keys_2 = data_2.keys()
+	common_keys = before_keys & after_keys
+	for key in removed_keys:
+		accumulator[key] = {
+			"_value": before[key],
+			"_status": "removed"
+		}
+	for key in added_keys:
+		accumulator[key] = {
+			"_value": after[key],
+			"_status": "added"
+		}
+	for key in common_keys:
+		if before[key] == after[key]:
+			accumulator[key] = {
+				"_value": before[key],
+				"_status": "unchanged"
+			}
+		elif isinstance(before[key], dict) and isinstance(after[key], dict):
+			accumulator[key] = {
+				**get_tree_with_categories(before[key], after[key]),
+				"_status": "recursive"
+			}
+		else:
+			accumulator[key] = {
+				"_value": before[key],
+				"_new_value": after[key],
+				"_status": "changed"
+			}
 
-    result = {}
-
-    for k in keys_1:
-
-        if k in keys_2:
-
-            if data_1[k] == data_2[k]:
-
-                val = data_1[k]
-
-                result[k] = val
-
-    return result
-
-
-def find_removed_data(data_1, data_2):
-
-    keys_1 = data_1.keys()
-    keys_2 = data_2.keys()
-
-    result = {}
-
-    for k in keys_1:
-
-        if k not in keys_2:
-
-            val = data_1[k]
-
-            result[k] = val
-    
-    return result
-
-
-def find_added_data(data_1, data_2):
-
-    keys_1 = data_1.keys()
-    keys_2 = data_2.keys()
-
-    result = {}
-
-    for k in keys_2:
-
-        if k not in keys_1:
-
-            val = data_2[k]
-
-            result[k] = val
-    
-    return result
-
-
-def find_changed_data(data_1, data_2):
-
-    keys_1 = data_1.keys()
-    keys_2 = data_2.keys()
-
-    before_change = {}
-    after_change = {}
-
-    for k in keys_1:
-
-        if k in keys_2:
-
-            if data_1[k] != data_2[k]:
-
-                val1 = data_1[k]
-                val2 = data_2[k]
-
-                before_change[k] = val1
-                after_change[k] = val2
-
-    return before_change, after_change
-
-
-def get_categorized_data(data_1, data_2):
-
-    same_data = find_the_same_data(data_1, data_2)
-    removed_data = find_removed_data(data_1, data_2)
-    added_data = find_added_data(data_1, data_2)
-    before_change, after_change = find_changed_data(data_1, data_2)
-
-    return same_data, removed_data, added_data, before_change, after_change
+	return accumulator

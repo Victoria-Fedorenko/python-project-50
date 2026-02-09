9894543c67717bdd03a371c9ad495fe653c5f344
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2026-02-09 21:58:42 +0300

    modified tests, tests data and expected results

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,8 +1,8 @@
 from gendiff.scripts.gendiff import generate_diff
-from gendiff.formatters.stylish import stylish
-# from gendiff.formatters.plain import plain
-# import json
-# from gendiff.formatters.json import json_formatter
+from gendiff.formatters.stylish import stylish_format_diff
+from gendiff.formatters.plain import plain_format_diff
+import json
+from gendiff.formatters.json import json_format_diff
 
 
 def load_expected(name):
@@ -14,21 +14,22 @@ def load_expected(name):
 
 def test_generate_diff():
     expected_stylish_short = load_expected('stylish_short.txt')
-    # expected_plain = load_expected('plain.txt')
-    # expected_json = json.loads(load_expected('json.json'))
+    expected_stylish_long = load_expected('stylish_long.txt')
+    expected_plain = load_expected('plain.txt')
+    expected_json = json.loads(load_expected('json.json'))
 
-    assert generate_diff('tests/test_data/file1.json', 
-                         'tests/test_data/file2.json') == expected_stylish_short
-    assert generate_diff('tests/test_data/file1.json', 
-                         'tests/test_data/file2.json', 
-                         stylish) == expected_stylish_short
+    assert generate_diff('tests/test_data/long1.json', 
+                         'tests/test_data/long2.json') == expected_stylish_long
+    assert generate_diff('tests/test_data/long1.json', 
+                         'tests/test_data/long2.json', 
+                         stylish_format_diff) == expected_stylish_long
     assert generate_diff('tests/test_data/filepath1.yml', 
                          'tests/test_data/filepath2.yml') == expected_stylish_short
-    # assert generate_diff('tests/test_data/file1.json', 
-    #                      'tests/test_data/file2.json', 
-    #                      plain) == expected_plain
-    # assert json.loads(generate_diff(
-    #                     'tests/test_data/file1.json', 
-    #                      'tests/test_data/file2.json', 
-    #                      json_formatter)) == expected_json
+    assert generate_diff('tests/test_data/long1.json', 
+                         'tests/test_data/long2.json', 
+                         plain_format_diff) == expected_plain
+    assert json.loads(generate_diff(
+                        'tests/test_data/long1.json', 
+                         'tests/test_data/long2.json', 
+                         json_format_diff)) == expected_json
     
